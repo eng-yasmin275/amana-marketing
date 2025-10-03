b543e7970aa2983b9ff0b820344dcd6eb5a632e4
Yasmin Nassar <eng.yasmin2751982@gmail.com>
2025-10-04 00:01:43 +0300

    Add CSP headers for Leaflet

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,33 @@
 import type { NextConfig } from "next";
 
+
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: securityHeaders,
+      },
+    ];
+  },
 };
 
+const securityHeaders = [
+  {
+    key: "Content-Security-Policy",
+    value: `
+      default-src 'self';
+      script-src 'self' 'unsafe-inline' 'unsafe-eval' https://unpkg.com https://*.tile.openstreetmap.org;
+      style-src 'self' 'unsafe-inline' https://unpkg.com https://*.tile.openstreetmap.org;
+      img-src * blob: data:;
+      connect-src *;
+      font-src 'self' data:;
+    `.replace(/\s{2,}/g, " ").trim(),
+  },
+];
+
+
+
 export default nextConfig;
+
